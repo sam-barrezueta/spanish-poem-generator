123ca6e58c65147d3207350209ce68561b61dbb3
sam-barrezueta <72712443+sam-barrezueta@users.noreply.github.com>
2025-03-03 16:17:53 +0000

    Add AI API integreation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-function generatePoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
+    console.log(response.data.answer);
 
     new Typewriter('#poem', {
-        strings: ["No hay nadie que, al dar un beso,"],
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
     });
 }
+
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let poemTopic = document.querySelector("#poem-topic");
+    let apiKey = "5477t35ad40f43989809bfo4a0ef16c2";
+    let prompt = `Generate a Spanish  poem about ${poemTopic.value}`;
+    let context = "You are a romantic poet. Your poems are four lines only and written in basic HTML, but do not include HTML in your response. You sign your poems at the end with 'Shecodes AI' inside a <strong> element.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log('Generating poem');
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiUrl).then(displayPoem);
+
+}
+
 
 
 
